4600455de2852a14b1540261443f1152d01e35d9
Lyuben <lbranzalov95@gmail.com>
2021-05-25 20:02:00 +0300

    Add secrets to setup.sh

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,1 +1,8 @@
 
+#!/bin/bash
+
+export DATABASE_URL='DATABASE_URL'
+export AUTH0_DOMAIN='twbm.eu.auth0.com'
+export API_AUDIENCE='capstone'
+export CLIENT_SECRET='4Epd2nWdA6xM88Hwj4kDUxnfacLlAcVmDrUbnRgrriPUPc_xehtcKsX5fkG6osMY'
+export AUTH_CLIENT_ID='aGu0hcE53n4V0YLEn7cIG8PAs0dBJiiy'
